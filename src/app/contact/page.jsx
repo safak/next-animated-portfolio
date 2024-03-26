9abb9da3ccf7d34646917fc233f1d8bd7111b12d
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-03-26 08:34:50 -0700

    added stuffs in the ordder page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,7 +10,7 @@ const contactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-12 lg:px-20 xl:px-48">
+      <div className="h-full overflow-scroll flex gap-6 flex-col lg:flex-row px-4 sm:px-12 lg:px-20 xl:px-48">
         {/*HELLO CONTAINER*/}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
           <div>
@@ -32,20 +32,25 @@ const contactPage = () => {
             👞
           </div>
         </div>
-       
 
         {/*FORM CONTAINER*/}
-        <form className="h-1/2 lg:h-full lg:w-1/2 bg-gradient-to-b from-yellow-300 to-black rounded-xl text-xl flex flex-col gap-8 justify-center p-24 mt-3">
-        <span>Dear Ps Customers</span>
-    <textarea rows={2} className="bg-transparent border-b-2 border-b-black outline-none resize-none"/>
-        <span>Our email address is</span>
-        <input type="text" className="bg-transparent border-b-2 border-b-black outline-none resize-none"/>
-        <span>Regards</span>
-        <button className="bg-blue-700 rounded font-semibold text-white p-4">Send</button>
+        <form className="h-[430px] lg:h-full lg:w-1/2 bg-gradient-to-b from-yellow-300 to-black rounded-xl text-xl flex flex-col gap-8 justify-center p-24 sm:p-36 mt-3">
+          <span>Dear Ps Customers</span>
+          <textarea
+            rows={2}
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+          />
+          <span>Our email address is</span>
+          <input
+            type="text"
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+          />
+          <span>Regards</span>
+          <button className="bg-blue-700 rounded font-semibold text-white p-4">
+            Send📩
+          </button>
         </form>
-
-        </div>
-      
+      </div>
     </motion.div>
   );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,42 +1,57 @@
-"use client"
-import {motion} from "framer-motion"
+"use client";
+import { motion } from "framer-motion";
 
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const HomePage = () => {
   return (
-    <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
-    <div className="h-full flex flex-col px-4 gap-4 lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col px-4 gap-4 lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/*image container*/}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative mt-4">
+          <Image
+            src="/padisco.png"
+            alt="hero"
+            fill
+            className="object-contain"
+          />
+        </div>
+        {/*end of image container*/}
 
-    {/*image container*/}
-    <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-    <Image src="/padisco.png" alt="hero" fill className="object-contain" />
-    </div>
-    {/*end of image container*/}
+        {/*text container*/}
+        <div className="mt-9 h-1/2  lg:w-1/2 lg:h-full flex flex-col gap-5 items-center justify-center m-auto md:mt-0 text-center lg:text-left">
+          {/*TITLE*/}
+          <h1 className="text-2xl font-bold md:text-4xl">
+            Creating Fashion Experience, Designing Tomorrow
+          </h1>
 
-    {/*text container*/}
-    <div className="mt-9 h-1/2  lg:w-1/2 lg:h-full flex flex-col gap-5 items-center justify-center m-auto md:mt-0 text-center lg:text-left">
-    
-    {/*TITLE*/}
-    <h1 className="text-2xl font-bold md:text-4xl">Creating Fashion Experience, Designing Tomorrow</h1>
+          {/*DESCRIPTION*/}
+          <p className="text-white text-sm md:text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam
+            tempore suscipit esse eveniet eos onsectetur adipisicing elit. Eum .
+          </p>
 
-    {/*DESCRIPTION*/}
-    <p className="text-white text-sm md:text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam tempore suscipit esse eveniet eos onsectetur adipisicing elit. Eum .</p>
+          {/*buttons*/}
+          <div className="lg:w-full flex gap-4">
+            <button className="text-sm p-2 lg:p-4 rounded-lg ring-1 ring-black bg-white text-black">
+              View Our Work
+            </button>
+            <button className="text-sm p-2 lg:p-4 rounded-lg ring-1 text-white bg-blue-700">
+              Contact Us
+            </button>
+          </div>
+        </div>
 
-    {/*buttons*/}
-    <div className="lg:w-full flex gap-4">
-    <button className="text-sm p-2 lg:p-4 rounded-lg ring-1 ring-black bg-white text-black">View Our Work</button>
-    <button className="text-sm p-2 lg:p-4 rounded-lg ring-1 text-white bg-blue-700">Contact Us</button>
-
-    </div>
-    </div>
-
-    {/*end of text container*/}
-      
-    </div>
+        {/*end of text container*/}
+      </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
